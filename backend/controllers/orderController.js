55ca5d0dedc36aae8b2263db5c680e4432782688
MokanRaamEL5870 <139581728+MokanRaamEL5870@users.noreply.github.com>
2024-10-08 21:45:51 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,10 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const currency = "inr";
+const deliveryCharge = 50;
+const frontend_URL = "https://food-delivery-app-frontend-t86p.onrender.com/";
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
 
